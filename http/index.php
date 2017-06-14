b93a971eec315ea721070d5b7d9cd7311763ffb8
Aleksandr S Melnik <alexmelnik224@gmail.com>
2017-06-14 12:01:11 -0700

    Add files via upload

---
+++ http/index.php
@@ -0,0 +1,138 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Romeo.ioe </title>
+   <link rel="stylesheet" href="css/custom.css">
+	    <style>
+
+img {
+	display: inline;
+    max-width: 100%;
+    height: auto;o
+}
+
+img:hover {
+     opacity: 0.5;
+}
+
+.dropbtn {
+    background-color: darkgray;
+    color: white;
+    padding: 16px;
+    font-size: 16px;
+    border: none;
+    cursor: pointer;
+}
+
+.dropdown {
+    position: relative;
+    display: inline-block;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    right: 0;
+    background-color: #f9f9f9;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+}
+
+.dropdown-content a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+}
+
+.dropdown-content a:hover {background-color: darkgray}
+
+.dropdown:hover .dropdown-content {
+    display: block;
+}
+
+.dropdown:hover .dropbtn {
+    background-color: darkgray;
+}
+</style>
+
+</head>
+<body>
+
+<div class="dropdown" style="float:left;">
+  <button class="dropbtn">Helpful Sites</button>
+  <div class="dropdown-content" style="left:0;">
+    <a href="https://www.w3schools.com/">W3Schools</a>
+    <a href="https://www.khanacademy.org/">Khanacademy</a>
+    <a href="https://www.codecademy.com/">Codecademy</a>
+    <a href="http://tritechsc.org/">Tritechsc</a>
+  </div>
+</div>
+
+<div class="dropdown" style="float:right;">
+  <button class="dropbtn">Information</button>
+  <div class="dropdown-content">
+    <a href="/2048">2048</a>
+    <a href="info.html">Info</a>
+    <a href="contact.html">Contact</a>
+  </div>
+</div>	
+	<center>
+<a href="http://10.183.2.18:8080" target = "_blank" class = "menu1" >:8080</a>
+<a href="http://10.183.2.18:3000" target = "_blank"class = "menu1" >:3000</a>
+<a href="downloads/" class = "menu1">downloads</a>
+<a href="NotMyAdmin/" class = "menu1">pma</a>
+<a href="logs/" target = "_blank" class = "menu1" >logs</a>
+<a href="docs/" target = "_blank" class = "menu1" >docs</a>
+<a href="https://github.com/MelnikAleksandr?tab=repositories" target = "_blank" class = "menu1" >github</a>
+<a href="html/" target = "_blank" class = "menu1" >html</a>
+<br />
+<img src = "img/img1.png" style="width:100px;height:100px;">
+<img src = "img/img2.png" style="width:100px;height:100px;">
+<a href = "https://sites.google.com/a/tritechsc.org/aleksandrm/" target = "_blank"><img src = "img/google.png"style="width:100px;height:100px;"></a>
+<img src = "img/img1.png" style="width:100px;height:100px;">
+<img src = "img/img2.png" style="width:100px;height:100px;">
+<br />
+<a href = "html/" target = "_blank"><img src = "img/html.png" style="width:100px;height:100px;"></a>
+<a href = "php/" target = "_blank"><img src = "img/php.png" style="width:100px;height:100px;"></a>
+<a href = "app/" target = "_blank"><img src = "img/phpapp.png "style="width:100px;height:100px;"></a>
+<a href = "js/" target = "_blank"><img src = "img/js.png " style="width:100px;height:100px;"></a>
+<a href = "code/" target = "_blank"><img src = "img/code.png" style="width:100px;height:100px;"></a>
+<br />
+<img src = "img/img2.png" style="width:100px;height:100px;">
+<img src = "img/img1.png" style="width:100px;height:100px;">
+<img src = "img/img2.png" style="width:100px;height:100px;">
+<img src = "img/img1.png" style="width:100px;height:100px;">
+<img src = "img/img2.png" style="width:100px;height:100px;">
+<br />
+<img src = "img/img1.png" style="width:100px;height:100px;">
+<img src = "img/img2.png" style="width:100px;height:100px;">
+<img src = "img/img1.png" style="width:100px;height:100px;">
+<img src = "img/img2.png" style="width:100px;height:100px;">
+<img src = "img/img1.png" style="width:100px;height:100px;">
+<br />
+<img src = "img/img2.png" style="width:100px;height:100px;">
+<img src = "img/img1.png" style="width:100px;height:100px;">
+<img src = "img/img2.png" style="width:100px;height:100px;">
+<img src = "img/img1.png" style="width:100px;height:100px;">
+<img src = "img/img2.png" style="width:100px;height:100px;">
+<br />
+<img src = "img/img1.png" style="width:100px;height:100px;">
+<img src = "img/img2.png" style="width:100px;height:100px;">
+<img src = "img/img1.png" style="width:100px;height:100px;">
+<img src = "img/img2.png" style="width:100px;height:100px;">
+<img src = "img/img1.png" style="width:100px;height:100px;">
+<pre>
+
+
+
+
+
+
+</pre>
+
+
+</body>
+</html>
